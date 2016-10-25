68716082066960eb1903d14dec9ed01580232e10
David Diers <david.diers@fourkitchens.com>
2016-10-25 18:53:24 -0500

    Further class fixes.

--- test/version-attribute.js
+++ test/version-attribute.js
@@ -73,35 +73,39 @@ const versionedSuperObject = {
   }
 };
 
-const versionedAttribute = new VersionAttribute([3, 1], versionedSuperObject);
+let versionedAttribute = new VersionAttribute([3, 1], versionedSuperObject);
 
 module.exports = {
+
   descendPath: function descendPathTest(test) {
     test.expect(5);
+    versionedAttribute = new VersionAttribute([3, 1], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.descendPath([3, 1], versionedSuperObject),
+      versionedAttribute.descendPath([3, 1]),
       [3, 1, 1, 1],
       'Paths with deep children sure go the entire way.'
     );
-
+    versionedAttribute = new VersionAttribute([2], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.descendPath([2], versionedSuperObject),
+      versionedAttribute.descendPath([2]),
       [2, 0, 1],
       'Paths at base should go the entire way.'
     );
-
+    versionedAttribute = new VersionAttribute([2, 0, 1], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.descendPath([2, 0, 1], versionedSuperObject),
+      versionedAttribute.descendPath([2, 0, 1]),
       [],
       'Paths at the deepest level should return an empty array.'
     );
+    versionedAttribute = new VersionAttribute([3], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.descendPath([3], versionedSuperObject),
+      versionedAttribute.descendPath([3]),
       [3, 1, 1, 1],
       'Base paths should move to the deepest level.'
     );
+    versionedAttribute = new VersionAttribute([3, 0], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.descendPath([3, 0], versionedSuperObject),
+      versionedAttribute.descendPath([3, 0]),
       [3, 0, 1, 1],
       'Base paths should move to the deepest level.'
     );
@@ -109,43 +113,51 @@ module.exports = {
   },
   ascendPath: function ascendPathTest(test) {
     test.expect(8);
+    versionedAttribute = new VersionAttribute([3, 0, 1], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.ascendPath([3, 0, 1], versionedSuperObject),
+      versionedAttribute.ascendPath([3, 0, 1]),
       [3, 0, 0],
       'Paths at or below target at the targets length should return true.'
     );
+    versionedAttribute = new VersionAttribute([3, 1], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.ascendPath([3, 1], versionedSuperObject),
+      versionedAttribute.ascendPath([3, 1]),
       [3, 0],
       'Sub paths of the target should return true.'
     );
+    versionedAttribute = new VersionAttribute([3, 0, 0], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.ascendPath([3, 0, 0], versionedSuperObject),
+      versionedAttribute.ascendPath([3, 0, 0]),
       [3, 0],
       'Base paths should go down a level'
     );
+    versionedAttribute = new VersionAttribute([0, 0, 0], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.ascendPath([0, 0, 0], versionedSuperObject),
+      versionedAttribute.ascendPath([0, 0, 0]),
       [],
       'Non existant zero paths should return empty.'
     );
+    versionedAttribute = new VersionAttribute([1, 0, 0], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.ascendPath([1, 0, 0], versionedSuperObject),
+      versionedAttribute.ascendPath([1, 0, 0]),
       [1, 0],
       'A non existant path should resolve to an existing parent if available.'
     );
+    versionedAttribute = new VersionAttribute([3, 1, 1, 1], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.ascendPath([3, 1, 1, 1], versionedSuperObject),
+      versionedAttribute.ascendPath([3, 1, 1, 1]),
       [3, 1, 1],
       'Base paths should go down a level'
     );
+    versionedAttribute = new VersionAttribute([4], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.ascendPath([4], versionedSuperObject),
+      versionedAttribute.ascendPath([4]),
       [3],
       'Non existant path at the first level should resolve to next item at first level.'
     );
+    versionedAttribute = new VersionAttribute([1], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.ascendPath([1], versionedSuperObject),
+      versionedAttribute.ascendPath([1]),
       [],
       'A final base bath with no sibling above should return empty.'
     );
@@ -156,27 +168,15 @@ module.exports = {
     test.expect(3);
     // Empty version returns error.
     test.throws(() => {
-      Object.create(VersionAttribute, {
-        pathCrumbs: [],
-        tree: versionedSuperObject,
-        target: 'members'}
-      );
-    }, 'Invalid path did not raise exception.');
+      new VersionAttribute('', versionedSuperObject, 'members'); // eslint-disable-line no-new
+    }, 'Empty path did not raise exception.');
     // Invalid version returns error.
     test.throws(() => {
-      Object.create(VersionAttribute, {
-        pathCrumbs: ['a', 4],
-        tree: versionedSuperObject,
-        target: 'members'}
-      );
+      new VersionAttribute(['a', 4], versionedSuperObject, 'members'); // eslint-disable-line no-new
     }, 'Invalid path did not raise exception.');
     // Invalid object returns error.
     test.throws(() => {
-      Object.create(VersionAttribute, {
-        pathCrumbs: [1, 0],
-        tree: {},
-        target: 'members'}
-      );
+      new VersionAttribute([1, 0], {}, 'members'); // eslint-disable-line no-new
     }, 'Empty object did not raise exception.');
     // Existing version path returns expected path
     test.done();
@@ -184,32 +184,38 @@ module.exports = {
   getPreviousClosestVersion: function getPreviousClosestVersionTest(test) {
     test.expect(6);
     // Empty version returns error.
+    versionedAttribute = new VersionAttribute([1, 0, 1], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.getPreviousClosestVersion([1, 0, 1], versionedSuperObject),
+      versionedAttribute.getPreviousClosestVersion([1, 0, 1]),
       [1, 0],
       'Base paths should go down a level'
     );
+    versionedAttribute = new VersionAttribute([2, 0], versionedSuperObject);
     test.deepEqual(
-      versionedAttribute.getPreviousClosestVersion([2, 0], versionedSuperObject),
+      versionedAttribute.getPreviousClosestVersion([2, 0]),
       [1, 0, 1],
       'Base paths should go up and then down to get next closest.'
     );
+    versionedAttribute = new VersionAttribute([3, 1, 1, 1], versionedSuperObject);
     // Empty version returns error.
     test.deepEqual(
-      versionedAttribute.getPreviousClosestVersion([3, 1, 1, 1], versionedSuperObject),
+      versionedAttribute.getPreviousClosestVersion([3, 1, 1, 1]),
       [3, 1, 1],
       'Deeply nested paths should traverse upward until they find a matching property.'
     );
+    versionedAttribute = new VersionAttribute([1], versionedSuperObject);
     test.deepEqual(
       versionedAttribute.getPreviousClosestVersion([1], versionedSuperObject),
       [],
       'No property at any path on or above should return empty.'
     );
+    versionedAttribute = new VersionAttribute([2, 0, 17], versionedSuperObject);
     test.deepEqual(
       versionedAttribute.getPreviousClosestVersion([2, 0, 17], versionedSuperObject),
       [2, 0, 1],
       'Non Existant sibling returns correct sibling.'
     );
+    versionedAttribute = new VersionAttribute([4], versionedSuperObject);
     test.deepEqual(
      versionedAttribute.getPreviousClosestVersion([4], versionedSuperObject),
       [3, 1, 1, 1],
